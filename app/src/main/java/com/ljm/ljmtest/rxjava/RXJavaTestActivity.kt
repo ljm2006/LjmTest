56452594a5c412d27e67afadeeff3c425f5b50a2
ljm2006 <ardentjm@gmail.com>
2020-11-17 19:28:21 +0900

    rxJava 예시 작업

--- app/src/main/java/com/ljm/ljmtest/rxjava/RXJavaTestActivity.kt
+++ app/src/main/java/com/ljm/ljmtest/rxjava/RXJavaTestActivity.kt
@@ -5,10 +5,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ljm.ljmtest.R
 import com.ljm.ljmtest.common.LjmUtil
-import io.reactivex.rxjava3.core.BackpressureStrategy
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.core.FlowableEmitter
-import io.reactivex.rxjava3.core.FlowableOnSubscribe
+import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.functions.Action
 import io.reactivex.rxjava3.functions.Consumer
 
 
@@ -17,8 +15,21 @@ class RXJavaTestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_java_test)
 
+        //Flowable, Observable -> rx java의 소스형태
+
         Flowable.fromArray("Hello Rx java!","My name is ljm").subscribe {
             LjmUtil.D(it)
         }
+
+        Observable.just("hello world!").subscribe {
+            LjmUtil.D(it)
+        }
+
+        Observable.just("하얀 늑대는 100의 데미지를 입었다.")
+            .map { "화살공격으로 인하여 " + it }
+            .map { it + "(25% 추가로 데미지를 받음)" }
+            .subscribe{
+                LjmUtil.D(it)
+            }
     }
 }
