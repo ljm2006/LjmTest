031e7b0339b662511dff627adf1a89bd8f4b0aec
ljm2006 <ardentjm@gmail.com>
2020-12-02 10:46:58 +0900

    Calculator 객체 생성시점에 구독하는 예시 추가

--- app/src/main/java/com/ljm/ljmtest/rxjava/RxCalculator.kt
+++ app/src/main/java/com/ljm/ljmtest/rxjava/RxCalculator.kt
@@ -6,12 +6,14 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.PublishSubject
 import io.reactivex.rxjava3.subjects.Subject
 
-class RxCalculator constructor(private val num1:Int, private val num2:Int) {
+class RxCalculator constructor(val num1:Int, val num2:Int) {
 
     private val subjectAdd : Subject<Pair<Int,Int>> = PublishSubject.create()
     private var result = 0
 
     private var targetTextView : TextView? = null
+
+    private val subjectCal : Subject<RxCalculator> = PublishSubject.create()
 
     init {
         subjectAdd.map { it.first + it.second }.subscribe {
@@ -22,11 +24,26 @@ class RxCalculator constructor(private val num1:Int, private val num2:Int) {
                 targetTextView?.text = result.toString()
             }
         }
+
+        //객체 생성하는 시점에서 구독 수행
+        subjectCal.subscribe {
+            with(it){
+                calculateSubtraction()
+            }
+        }
+
+        subjectCal.onNext(this)
     }
 
     fun calculate(resultTextView:TextView){
         targetTextView = resultTextView
         val pair = Pair(num1, num2)
         subjectAdd.onNext(pair)
+    }
+
+    inline fun calculateSubtraction() : Int{
+        val result = num1 - num2
+        LjmUtil.D("sub : $result")
+        return result
     }
 }
